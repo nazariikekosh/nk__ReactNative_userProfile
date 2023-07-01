e03b910f26e6c1ab3b4888d672d73b177224686b
nazariikekosh <118050634+nazariikekosh@users.noreply.github.com>
2023-07-01 19:31:22 +0300

    add fonts

--- userProfile/App.js
+++ userProfile/App.js
@@ -1,10 +1,15 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React from 'react';
+import { Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+
 
 export default function App() {
   return (
-    <View>
-      <Text>App</Text>
-    </View>
+   <NavigationContainer>
+      <Text>hello</Text>
+   </NavigationContainer>
   )
 }
